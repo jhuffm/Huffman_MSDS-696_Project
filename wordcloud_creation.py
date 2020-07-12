18c39347a17289c0de63749fc603e415dcf71966
jhuffm <65859364+jhuffm@users.noreply.github.com>
2020-07-12 13:49:19 -0600

    Update wordcloud_creation.py

--- wordcloud_creation.py
+++ wordcloud_creation.py
@@ -7,8 +7,6 @@ import matplotlib.pyplot as plt
 data_pull = pd.read_csv('C:/Users/huffm/tweets.csv', header=None)
 raw_tweets = data_pull.iloc[:,1]
 
-badwords = ['fuck', 'fucking', 'shit', 'bitch', 'ass', 'damn', 'bullshit', 'fucked']
-
 # eliminate special characters, links, short words, and stopwords
 raw_string = ''.join(raw_tweets)
 no_links = re.sub(r'http\S+', '', raw_string)
@@ -19,10 +17,8 @@ words = no_special_characters.split(" ")
 words = [w for w in words if len(w) > 2]
 words = [w.lower() for w in words]
 words = [w for w in words if w not in STOPWORDS]
-words = [w for w in words if w not in badwords]
  
 #print(words)
-
 wc = WordCloud(background_color='white',
         width=1600,
         height=800,
@@ -39,8 +35,3 @@ plt.axis('off')
 plt.imshow(wc, interpolation="bilinear")
 plt.title("Colorado Twitter Word Cloud")
 plt.show()
-
-
-#https://medium.com/@shsu14/introduction-to-data-science-custom-twitter-word-clouds-704ec5538f46
-#https://towardsdatascience.com/almost-real-time-twitter-sentiment-analysis-with-tweep-vader-f88ed5b93b1c
-
