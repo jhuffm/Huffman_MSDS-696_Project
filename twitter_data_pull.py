8b91b440b89457486eade954486d1dd4bec5853c
jhuffm <65859364+jhuffm@users.noreply.github.com>
2020-07-19 17:44:38 -0600

    Update twitter_data_pull.py

--- twitter_data_pull.py
+++ twitter_data_pull.py
@@ -1,32 +1,66 @@
-import os
-import tweepy as tw
-import pandas as pd
+import GetOldTweets3 as got
 import csv
-import twitter
+import pandas as pd
+import time
 
-# Authenticate with Twitter
-consumer_key= "consumer_key"
-consumer_secret= "consumer_secret"
-access_token= "access_token"
-access_token_secret= "access_token_secret"
+# Variables
+max_tweets = 13000
 
-# Using Tweepy
-auth = tw.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tw.API(auth, wait_on_rate_limit=True)
+#csvFile = open('tweetscolorado.csv', 'a', newline='')
+#csvWriter = csv.writer(csvFile)
 
-# Search Terms
-search_words = "*"
-cnt = 1000
-geo = '38.99903,-105.5459,190mi'
+def namestr(obj, namespace):
+        return [name for name in namespace if namespace[name] is obj]
 
-# Collect Tweets
-# Collect Tweets and write to CSV using Tweepy and CSVWriter
-csvFile = open('tweets.csv', 'a')
-csvWriter = csv.writer(csvFile)
+def get_tweets(date_since, date_until, search_term, max_tweets, near, within):
+    tweetCriteria = got.manager.TweetCriteria().setQuerySearch(search_term)\
+                                           .setSince(date_since)\
+                                           .setUntil(date_until)\
+                                           .setMaxTweets(max_tweets)\
+                                           .setNear(near)\
+                                           .setWithin(within)\
+                                           .setLang('en')
+    tweets = got.manager.TweetManager.getTweets(tweetCriteria)
+    for tweet in tweets:
+        tweet_text = tweet.text.encode('utf-8', 'ignore')
+        tweet_date = tweet.date
+        tweet_id = tweet.id
+        tweet_geo = tweet.geo
+        csvWriter.writerow([tweet_id, tweet_text, tweet_date, tweet_geo])
 
-for tweet in tw.Cursor(api.search,
-              q= search_words + "-filter:retweets",
-              lang="en",
-              geocode= geo).items(cnt):
-    csvWriter.writerow([tweet.created_at, tweet.text.encode('ascii', 'ignore'), tweet.user.id, tweet.user.location.encode('utf-8', 'ignore')])
+#get_tweets()
+# Create Cagegories of search terms
+masks = 'masks OR coverings OR face masks OR mask OR facemasks OR facemask OR ppe'
+social_distance = 'distance OR apart OR social distance OR isolation OR isolating OR socialdistancing OR stayathome OR lockdown OR stayhome OR staysafe'
+hand_washing = 'wash hands OR wash OR washing OR sanitize OR santizing OR washyourhands OR handsanitizer OR disinfect OR santise OR clean'
+
+# Create search grid
+search_grid = [masks, social_distance, hand_washing]
+
+# Load county info for centerpoint and radius of each Colorado county
+colorado_counties = pd.read_csv("C:/Users/huffm/Desktop/MSDS 696/data/colorado_counties.csv", index_col = "county")
+
+# Dates to pull
+# Pulling one day at a time to avoid having to start over if program encounters problems
+idx = pd.date_range(start="2020-03-15",end="2020-06-30").date
+idx2 = pd.date_range(start="2020-03-16",end="2020-07-01").date
+starting_dates = pd.DataFrame({'date_since':idx})
+ending_dates = pd.DataFrame({'date_until':idx2})
+dates = starting_dates.merge(ending_dates, left_index = True, right_index=True)
+
+# # iterate through each county creating a different .csv file for each county/search term combination
+for index, row in dates.iterrows():
+        date_since = str(row['date_since'])
+        date_until = str(row['date_until'])
+        
+        for i in search_grid:
+                search_term = i
+                name = namestr(search_term, globals())[0]
+        
+                for label, row in colorado_counties.iterrows():
+                        near = str(row["latitude"])+', '+ str(row["longitude"])
+                        within = str(row["geocode_radius"])+"mi"
+                        filename = name + '_' + label + ".csv"
+                        csvFile = open(filename, 'a', newline='')
+                        csvWriter = csv.writer(csvFile)
+                        get_tweets(date_since, date_until, search_term, max_tweets, near, within)
