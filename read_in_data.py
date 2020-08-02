369b9fd5269592f54322b4f18cc97503b8c48fb2
jhuffm <65859364+jhuffm@users.noreply.github.com>
2020-08-02 11:35:00 -0600

    Update read_in_data.py
    Removed lines that added county column since I decided to create model for a single Denver metro area rather than for every county.

--- read_in_data.py
+++ read_in_data.py
@@ -21,24 +21,9 @@ combined_twitter_data['search_term'] = np.where(a == 'C:/Users/huffm\hand', 'hyg
     np.where(a == 'C:/Users/huffm\soci', 'distancing',
     np.where(a == 'C:/Users/huffm\coro', 'coronavirus', 0))))
 
-# Add a column for the name of the county pulling the information from the filename
-county_data = pd.read_csv("C:/Users/huffm/Desktop/MSDS 696/data/colorado_counties.csv")
-combine_statement = ""
-for county in county_data['county']:
-    length = len(county) + 4
-    filename_string = "combined_twitter_data.filename.str[-"+str(length)+":]"
-    parameter = county + '.csv'
-    variable = "np.where("+filename_string+" == '"+parameter+"','"+county+"', "
-    combine_statement = combine_statement + variable
-
-paren_number =  ")" * 64
-final_combine_statement = combine_statement + " 0" + str(paren_number)
-combined_twitter_data['county'] = eval(final_combine_statement)
-
 # Remove unnecessary columns and add appropriate column headers
 combined_twitter_data.drop(combined_twitter_data.columns[[0, 4]], axis = 1, inplace = True)
-combined_twitter_data.columns = ['id', 'tweet_text', 'filename', 'date_time', 'search_term', 'county']
+combined_twitter_data.columns = ['id', 'tweet_text', 'date_time', 'filename', 'search_term']
 
 # Save new dataframe to a csv file
 combined_twitter_data.to_csv("C:/Users/huffm/combined_twitter_data.csv")
-print(combined_twitter_data.head())
