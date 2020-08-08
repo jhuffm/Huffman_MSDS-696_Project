a9ad80a37558837ef99132ac4c22878ee975db82
jhuffm <65859364+jhuffm@users.noreply.github.com>
2020-08-08 12:57:38 -0600

    Update read_in_data.py

--- read_in_data.py
+++ read_in_data.py
@@ -3,8 +3,8 @@ from glob import glob
 import os
 import numpy as np
 
-# Select all csv files that have data for this project
-stock_files = sorted(glob("C:/Users/huffm/* County.csv"))
+# Select all csv files that have data for this project; must be in same file path or include filepath in glob
+stock_files = sorted(glob("* County.csv"))
 
 # Combine individual twitter pull files into single data frame
 dfList = []
@@ -14,16 +14,16 @@ for filename in stock_files:
         dfList.append(df)
 combined_twitter_data = pd.concat(dfList, axis = 0).reset_index()
 
-# Add a column for the search term used
-a = combined_twitter_data.filename.str[:19]
-combined_twitter_data['search_term'] = np.where(a == 'C:/Users/huffm\hand', 'hygiene',
-    np.where(a == 'C:/Users/huffm\mask', 'masks',
-    np.where(a == 'C:/Users/huffm\soci', 'distancing',
-    np.where(a == 'C:/Users/huffm\coro', 'coronavirus', 0))))
+# Add a column for the search term used extracted from the filename
+a = combined_twitter_data.filename.str[:4]
+combined_twitter_data['search_term'] = np.where(a == 'hand', 'hygiene',
+    np.where(a == 'mask', 'masks',
+    np.where(a == 'soci', 'distancing',
+    np.where(a == 'coro', 'coronavirus', 0))))
 
 # Remove unnecessary columns and add appropriate column headers
 combined_twitter_data.drop(combined_twitter_data.columns[[0, 4]], axis = 1, inplace = True)
 combined_twitter_data.columns = ['id', 'tweet_text', 'date_time', 'filename', 'search_term']
 
 # Save new dataframe to a csv file
-combined_twitter_data.to_csv("C:/Users/huffm/combined_twitter_data.csv")
+combined_twitter_data.to_csv("combined_twitter_data.csv")
