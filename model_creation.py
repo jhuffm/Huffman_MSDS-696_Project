8a7c66551223c478333b92f831852f7acf809dc2
jhuffm <65859364+jhuffm@users.noreply.github.com>
2020-08-08 13:12:42 -0600

    Update model_creation.py
    added model evaluation and Granger test

--- model_creation.py
+++ model_creation.py
@@ -10,16 +10,43 @@ from statsmodels.tsa.vector_ar.var_model import VAR
 from statsmodels.tsa.stattools import acf
 from math import sqrt
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
 
-merged_data = pd.read_csv("C:/Users/huffm/merged_data.csv", index_col = 0)
+merged_data = pd.read_csv("merged_data.csv", index_col = 0)
 
 merged_data['Date'] = pd.to_datetime(merged_data.Date , format = '%Y-%m-%d')
 data = merged_data.drop(['Date'], axis=1)
 data.index = merged_data.Date
 
-# Fill in missing values with preceeding value
-data = data.fillna(method='ffill')
+# Plotting Infection Rate
+merged_data = merged_data.drop(['Date', 'coronavirus_EMA', 'distancing_EMA', 'hygiene_EMA', 'masks_EMA'], axis = 1)
+sb.set_style('darkgrid')
+merged_data.plot(kind = 'line', legend = 'reverse', title = 'Infection Rate Time-Series')
+plt.legend(loc = 2, shadow = True)
+plt.tight_layout()
+plt.show()
+
+# check for missing values
+print(data.isnull().sum())
 print(data.shape)
+
+# Test for Causality using Granger causality test  ### Probably need to adjust
+maxlag=12
+test = 'ssr_chi2test'
+def grangers_causation_matrix(data, variables, test='ssr_chi2test', verbose=False):    
+    df = pd.DataFrame(np.zeros((len(variables), len(variables))), columns=variables, index=variables)
+    for c in df.columns:
+        for r in df.index:
+            test_result = grangercausalitytests(data[[r, c]], maxlag=maxlag, verbose=False)
+            p_values = [round(test_result[i+1][0][test][1],4) for i in range(maxlag)]
+            if verbose: print(f'Y = {r}, X = {c}, P Values = {p_values}')
+            min_p_value = np.min(p_values)
+            df.loc[r, c] = min_p_value
+    df.columns = [var + '_x' for var in variables]
+    df.index = [var + '_y' for var in variables]
+    return df
+
+print(grangers_causation_matrix(data, variables = data.columns))
 
 # Split Data into training and validation dataset
 # For time series we want to use continuous portions of the dataset rather than pulling a random 80% for training
@@ -61,7 +88,7 @@ for name, column in df_differenced.iteritems():
 
 # create and fit a model
 model = VAR(df_differenced)
-results = model.fit(maxlags=12, ic='aic')
+results = model.fit(maxlags=15, ic='aic')
 print(results.summary())
 
 # Forecast using model
@@ -86,6 +113,27 @@ def invert_transformation(df_train, df_forecast, second_diff=False):
         df_fc[str(col)+'_forecast'] = df_train[col].iloc[-1] + df_fc[str(col)+'_1d'].cumsum()
     return df_fc
 
-# Display real predictions
+# Display non-inverted predictions
 df_results = invert_transformation(training_data, df_forecast, second_diff=True)
-print(df_results.loc[:, ['Rate_forecast', 'coronavirus_forecast', 'distancing_forecast', 'hygiene_forecast', 'masks_forecast']])
+print(df_results.loc[:, ['Rate_forecast']])
+
+# Plot predictions vs actuals
+sb.set_style('darkgrid')
+df_results['Rate_forecast'].plot()
+data["Rate"].plot()
+plt.title(label = 'Rate Forecast vs Actuals')
+plt.legend(loc = 2, shadow = True)
+plt.tight_layout()
+plt.show()
+
+# Evaluate Accuracy of Model
+expected = validation_data['Rate']
+predicted = df_results['Rate_forecast'].values
+
+mae = mean_absolute_error(expected, predicted)
+mse = mean_squared_error(expected, predicted)
+rmse = sqrt(mse)
+print('Forecast Accuracy')
+print('MAE = %f'% mae)
+print('MSE = %f'% mse)
+print('RMSE = %f'% rmse)
